49ac32e32c7479aada9ebd8a72ebe0b6d0307ff3
NovaIZ01 <119665263+NovaIZ01@users.noreply.github.com>
2024-01-30 23:25:29 +0800

    Add project files.

---
+++ WebApplication3/Model/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Model
+{
+	public class ApplicationUser : IdentityUser
+	{
+
+		public string FullName { get; set; }
+
+		public string CreditCard { get; set; }
+
+		public string Gender { get; set; }
+
+		public string MobileNumber { get; set; }
+
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[DataType(DataType.Password)]
+		public string ConfirmPassword { get; set; }
+
+		public string DeliveryAddress { get; set; }
+
+		public string AboutMe { get; set; }
+
+	}
+}
---
+++ WebApplication3/ViewModels/Register.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace WebApplication3.ViewModels
+{
+    public class Register
+    {
+		[Required]
+		[DataType(DataType.Text)]
+		public string FullName { get; set; }
+
+		[Required]
+		[DataType(DataType.CreditCard)]
+        [RegularExpression("[0-9]{16,16}",
+            ErrorMessage = "Enter a valid Credit card number without any spacing or -"
+            )]
+        public string CreditCard { get; set; }
+
+		[Required]
+		public string Gender { get; set; }
+
+		[Required]
+		[DataType(DataType.PhoneNumber)]
+		[RegularExpression("[0-9]{8,8}",
+			ErrorMessage = "Enter a valid Phone Number that is 8 numbers long"
+			)]
+		
+		public string MobileNumber { get; set; }
+
+		[Required]
+		public string DeliveryAddress { get; set; }
+
+		[Required]
+		[RegularExpression("^[\\w\\-\\.]+@([\\w-]+\\.)+[\\w-]{2,}$",
+			ErrorMessage = "Enter a Valid Email")]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+		[Required]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{8,})\S$", 
+		ErrorMessage = "Password must be at least 12 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+        public string Password { get; set; }
+		[Required]
+		[DataType(DataType.Password)]
+		public string ConfirmPassword { get; set; }
+
+
+		public string AboutMe { get; set; }
+
+	}
+}
